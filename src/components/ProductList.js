19453589e70bed9ca1ba621f660e27134ce2c59d
Haidir Hassan <Studenthaidir@gmail.com>
2020-08-25 23:52:10 +0800

    context.js context API simple working

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Product from './Product'
 import Title from './Title'
 import { storeProducts } from '../data'
+import { ProductConsumer } from '../context'
 
 export default class ProductList extends Component {
     constructor(props) {
@@ -13,13 +14,19 @@ export default class ProductList extends Component {
     }
     
     render() {
-        console.log(this.state.products)
         return (
             <React.Fragment>
                 <div className="py-5">
                     <div className="container">
+                        <Title name="our" title="product" />
                         <div className="row">
-                            <Title name="our" title="product" />
+                            <ProductConsumer>
+                                {(value) => {
+                                    return value.products.map(product => {
+                                        return <Product key={product.id} product={product} />
+                                    })
+                                }}
+                            </ProductConsumer>
                         </div>
                     </div>
                 </div>
