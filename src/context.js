a6c6bda495640d880b2ab456e1690506d7a1b3c8
Haidir Hassan <Studenthaidir@gmail.com>
2020-08-27 00:14:03 +0800

    removeItem working

--- src/context.js
+++ src/context.js
@@ -34,6 +34,8 @@ class ProductProvider extends Component {
         })
         this.setState(() => {
             return {products: tempProducts}
+        }, () => {
+            this.addTotals()
         })
     }
     
@@ -87,11 +89,33 @@ class ProductProvider extends Component {
     }
 
     removeItem = (id) => {
-        console.log('item removed')
+        let tempProducts = [...this.state.products]
+        let tempCart = [...this.state.cart]
+        tempCart = tempCart.filter(item => item.id !== id)
+
+        const index = tempProducts.indexOf(this.getItem(id))
+        let removedProduct = tempProducts[index]
+        removedProduct.inCart = false
+        removedProduct.count = 0
+        removedProduct.total = 0
+
+        this.setState(() => {
+            return {
+                cart: [...tempCart],
+                products: [...tempProducts]
+            }
+        },
+        () => {
+            this.addTotals()
+        })
     }
 
     clearCart = () => {
-        console.log('cart was cleared')
+        this.setState(() => {
+            return { cart: [] }
+        }, () => {
+            this.setProducts()
+        })
     }
 
     addTotals = () => {
